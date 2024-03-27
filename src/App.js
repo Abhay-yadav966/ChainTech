e44a3648bcad465dc29985bd4849c452093029fe
Abhay Yadav <yadavabhay966@gmail.com>
2024-03-27 12:54:13 +0530

    Section 1 completed

--- src/App.js
+++ src/App.js
@@ -9,9 +9,22 @@ function App() {
       
       {/* border line */}
       <div className="border-b border-[#1F1F1F]" ></div>
-      {/* section 1 */}
-      <section>
 
+      {/* section 1 */}
+      <section className="w-11/12 max-w-7xl flex flex-col items-center mx-auto" >
+        {/* heading */}
+        <h1 className=" font-black text-[120px] text-white" >WELCOME</h1>
+        <h2 className=" font-black text-[120px] text-white " >TO <span className="font-extrabold text-[120px] bg-gradient-to-r from-[#00FFA7] to-[#FFFF00] text-transparent bg-clip-text " >CHAINTECH</span></h2>
+
+        {/* paragraph */}
+        <p className="font-medium text-lg text-white text-center w-3/5 " >From square one to engineering excellence! PixelPlex assists in full-cycle software development, jumps in to take it over, or brings dedicated top-demanded skills.</p>
+      </section>
+
+      {/* section 2 */}
+      <section className="bg-white" >
+        <div className="w-11/12 max-w-7xl mx-auto" >
+          
+        </div>
       </section>
     </div>
   );
