8702bdff7ae0692a28c434768e5aa24bc2260ec6
Abhay Yadav <yadavabhay966@gmail.com>
2024-03-27 12:08:24 +0530

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Navbar from "./Components/Core/Navbar";
 
 function App() {
   return (
-    <div className="overflow-x-hidden overflow-y-auto m-0 p-0 h-screen w-screen bg-black" >
+    <div className="overflow-x-hidden overflow-y-auto m-0 p-0 font-Poppins h-screen w-screen bg-black" >
       
       <Navbar/>
       
+      {/* border line */}
+      <div className="border-b border-[#1F1F1F]" ></div>
       {/* section 1 */}
       <section>
 
--- src/Components/Core/Navbar.jsx
+++ src/Components/Core/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../assets/Group.png'
 import {NavbarData} from '../../data/NavbarData'
 
 const Navbar = () => {
 
     // state variable for showing current tab
-
+    const [tab, setTab] = useState(NavbarData[0].title);
+    
   return (
-    <div>
+    <div className=' w-11/12 max-w-7xl mx-auto py-6 flex items-center' >
         {/* image */}
         <img src={Logo} alt="Logo Image" />
 
         {/* navbar */}
-        <nav>
-            <ul>
-
+        <nav className='mx-auto' >
+            <ul className='flex items-center gap-5' >
+                {
+                    NavbarData?.map((element) => (
+                        <li key={element?.id}
+                            onClick={() => setTab(element?.title)}
+                            className={`${tab === element?.title ? "text-[#00FFA7]" : "text-white"} font-semibold text-lg cursor-pointer`}
+                        >
+                            {element?.title}
+                        </li>
+                    ))
+                }
             </ul>
         </nav>
     </div>
